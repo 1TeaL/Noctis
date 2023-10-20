cf16b5cba023ada33fe3b26264bae74232d362b3
1TeaL <soxas.roxas@gmail.com>
2023-10-20 18:31:32 +1100

    sword combo checks in

--- NoctisVS/NoctisMod/SkillStates/Skills/Sword/SwordCombo.cs
+++ NoctisVS/NoctisMod/SkillStates/Skills/Sword/SwordCombo.cs
@@ -10,121 +10,98 @@ using R2API;
 
 namespace NoctisMod.SkillStates
 {
-    public class SwordCombo : BaseMeleeAttack
+    public class SwordCombo : BaseSkillState
     {
+        public NoctisController noctisCon;
+        private bool weaponSwap;
+
+        protected Vector2 inputVector;
+
         public override void OnEnter()
         {
 
-            //AkSoundEngine.PostEvent("ShiggyMelee", base.gameObject);
-
-            this.hitboxName = "DecayHitbox";
-
-            this.damageCoefficient = 1f;
-            this.procCoefficient = 1f;
-            this.pushForce = 0f;
-            this.damageType = DamageType.Generic;
-            this.baseDuration = 1f;
-            this.attackStartTime = 0.2f;
-            this.attackEndTime = 0.4f;
-            this.baseEarlyExitTime = 0.4f;
-            this.hitStopDuration = 0.08f;
-            this.attackRecoil = 0.75f;
-            this.hitHopVelocity = 7f;
-
-            this.swingSoundString = "ShiggyMelee";
-            this.hitSoundString = "";
-            this.muzzleString = ChooseAnimationString();
-            this.swingEffectPrefab = Modules.Assets.shiggySwingEffect;
-            this.hitEffectPrefab = Modules.Assets.shiggyHitImpactEffect;
-
-            this.impactSound = Modules.Assets.hitSoundEffect.index;
-
             base.OnEnter();
+            noctisCon = gameObject.GetComponent<NoctisController>();
+            Ray aimRay = base.GetAimRay();
 
-        }
-
-
-        private string ChooseAnimationString()
-        {
-            string returnVal = "Swing1";
-            switch (this.swingIndex)
+            //check weapon swap
+            if (noctisCon.weaponState != NoctisController.weaponType.NONE || noctisCon.weaponState != NoctisController.weaponType.SWORD)
             {
-                case 0:
-                    returnVal = "Swing1";
-                    break;
-                case 1:
-                    returnVal = "Swing2";
-                    break;
+                weaponSwap = true;
+            }
+            else
+            {
+                weaponSwap = false;
             }
 
-            return returnVal;
-        }
-
-
-        //public override void OnExit()
-        //{
-        //    base.OnExit();
-        //    //if (NetworkServer.active)
-        //    //{
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.acridBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.impbossBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.commandoBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.captainBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.loaderBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.greaterwispBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.claydunestriderBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.alphashieldonBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.beetleBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.minimushrumBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.larvajumpBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.pestjumpBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.vagrantBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.magmawormBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.gupspikeBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.hermitcrabmortarBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.impbossBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.lesserwispBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.lunarexploderBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.roboballminiBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.stonetitanBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.verminsprintBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.voidbarnaclemortarBuff);
-        //    //    base.characterBody.RemoveBuff(Modules.Buffs.voidjailerBuff);
-        //    //}
-        //}
-
-
-        protected override void PlayAttackAnimation()
-        {
-            base.PlayAttackAnimation();
-        }
-
-        protected override void PlaySwingEffect()
-        {
-            base.PlaySwingEffect();
-        }
-
-        protected override void OnHitEnemyAuthority()
-        {
-            base.OnHitEnemyAuthority();
 
         }
 
-        protected override void SetNextState()
+        public override void Update()
         {
-            int index = this.swingIndex;
-            if (index == 0) index = 1;
-            else index = 0;
+            base.Update();
 
-            this.outer.SetNextState(new SwordCombo
+            //check input
+            if (weaponSwap)
             {
-                swingIndex = index,
-            });
+                //weapon swap combos
+                if (!base.isGrounded)
+                {
+                    //aerial attack
+
+                }
+                else
+                {
+                    if (base.inputBank.moveVector == Vector3.zero)
+                    {
+                        //neutral attack
+                        Chat.AddMessage("neutral attack- swap");
+                    }
+                    else
+                    {
+                        Vector3 moveVector = base.inputBank.moveVector;
+                        Vector3 aimDirection = base.inputBank.aimDirection;
+                        Vector3 normalized = new Vector3(aimDirection.x, 0f, aimDirection.z).normalized;
+                        Vector3 up = base.transform.up;
+                        Vector3 normalized2 = Vector3.Cross(up, normalized).normalized;
+
+                        if (Vector3.Dot(base.inputBank.moveVector, normalized) >= 0.8f)
+                        {
+                            //forward attack
+                            Chat.AddMessage("forward attack- swap");
+                        }
+                        else if (Vector3.Dot(base.inputBank.moveVector, normalized) <= -0.8f)
+                        {
+                            //backward attack
+                            Chat.AddMessage("backward attack- swap");
+                        }
+                        else
+                        {
+                            //neutral attack
+                            Chat.AddMessage("neutral attack- swap");
+                        }
+
+                    }
+
+                }
+
+
+            }
+            else if (!weaponSwap)
+            {
+                //normal combos
+            }
         }
+
 
         public override void OnExit()
         {
             base.OnExit();
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.Skill;
         }
 
     }
